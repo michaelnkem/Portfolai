283485b534789c6fd46046a8ed34929f5aafdb22
michaelnkem <michael.nkem@hotmail.co.uk>
2026-06-03 12:07:15 +0100

    Update route.ts

--- src/app/api/debug/route.ts
+++ src/app/api/debug/route.ts
@@ -1,4 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
+
+export const dynamic = 'force-dynamic'
+
 export async function GET(req: NextRequest) {
   const outcode = req.nextUrl.searchParams.get('outcode') || 'EN3'
   const postcode = req.nextUrl.searchParams.get('postcode') || ''
